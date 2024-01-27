54e71e65954cbb45eb0980e64f5046dbfe2b245a
Tejas <tejasdalvi124b@gmail.com>
2024-01-27 22:13:40 +0530

    removed long introduction form homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,14 +11,11 @@ function Home() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ ease: "easeInOut", duration: 1.5, times: [0.1] }}
         className="introduction"
-      ><p className="circleBig">
-        <span className="bold">🚀🎨 Hello World!</span> I'm Tejas, a versatile software developer specializing in
-        frontend technologies like HTML, CSS, Javascript, and frameworks like React.js, Framer motion, material UI, etc. With a keen eye for design
-        and a knack for crafting seamless user experiences, I bring ideas to
-        life through clean and efficient code. Additionally, my journey extends
-        into the realm of blockchain, adding a unique dimension to my skill set.
-        Let's collaborate and turn your concepts into captivating digital
-        solutions!
+      >
+        <h1 className="circleBig">Hey, I am Tejas</h1>
+        <p className="circleBig">
+          A frontend developer who works with ReactJs and also curious about
+          blockchain technology!
         </p>
       </motion.div>
 
