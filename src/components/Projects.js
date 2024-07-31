9d3e42f29bc23408c4c3ef78b8f1d27b3ae7a459
Tejas <tejasdalvi124b@gmail.com>
2024-07-31 12:19:14 +0530

    added new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,14 +9,18 @@ import complaint from '../Images/complaint chain.png'
 import research from '../Images/research tool.png'
 import ghoEco from '../Images/gho eco cover image.png'
 import Partsign from '../Images/partsign1.png'
+import Haldiram from '../Images/haldiram.png'
+import ola from '../Images/ola.png'
 
 function Projects() {
   return (
     <div className="projects">
+      <ProjectCard name={"Haldiram UI Re-Imagine"} img={Haldiram} link={"https://angularbrackets2.netlify.app/"} description={"Recreated the Haldirams website with a new feel and great animatinos. It was A group project where I was the developer"}/>
       <ProjectCard name={"PartSign"} img={Partsign} link={"https://partsign.netlify.app/"} description={"This project solves the problem of computer parts scalping by using the sign protocol attestation service."}/>
       <ProjectCard name={"Gho-Eco"} img={ghoEco} link={"http://ghoeco.netlify.app/"} description={"Gho-Eco is a platform where we invite organizations from across the globe to showcase their eco-friendly initiatives. The best entries get rewarded with GHO Tokens as an appreciation"}/>
       <ProjectCard name={"Certificates Page"} img={certificate} link={"https://tejascertificates.netlify.app/"} description={"A full stack application where all of my certificates are displayed. Created an API from where i can upload new certificates. The certificates are stored in mongodb in base64 format and are fetched from the api and displayed on frontend"}/>
       <ProjectCard name={"Complaint Chain"} img={complaint} link={"http://complaintchain.tech/"} description={"Blockchain based Anonymous Complaining app where users can send complaints anonymously. The complaints are viewed by the respective authorities on the admin page. The admins can login through NFT Gating mechanism to access the admin page. (Note:- please install metamask wallet browser extension before using the project)"}/>
+      <ProjectCard name={"Ola UI Re-Imagine"} img={ola} link={"https://angularbrackets.netlify.app/"} description={"Recreated the OLA Cabs website with a new feel. It was A group project where I was the developer"}/>
       <ProjectCard name={"Cheemdiya List"} img={todo} link={"https://cheemdiyalist.netlify.app/"} description={"A To-Do list app"}/>
       <ProjectCard name={"BlueSky Donation"} img={donation} link={"https://cheemdiya.netlify.app/"} description={"Blockchain based platform for NGOs to receive donations in the form of cryptocurrency. The list of donors is displayed on the home page so that others get motivated. (Note:- please install metamask wallet browser extension before using the project)"}/>
       <ProjectCard name={"BlockShare"} img={blockhare} link={"https://blockshare2-1a8859.spheron.app/"} description={"Blockchain based image sharing platform. Share your files from anywhere in the world to everywhere in the world using Blockchain (Note:- please install metamask wallet browser extension before using the project)"}/>
